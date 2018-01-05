aa1c65795a0dd1d50946e93b4bd83c076672c398
TrentaIcedCoffee <jin@ucdavis.edu>
2018-01-05 15:14:55 -0800

    IMPL sigmoid sigmoidGradient

--- neural-network/debug.m
+++ neural-network/debug.m
@@ -2,6 +2,9 @@
 
 % this file is used to debug the following files
 %   displayData.m
+%   sigmoid.m
+%   sigmoidGradient.m
+%   randTheta.m
 
 %% Initialization
 close all;
@@ -17,3 +20,28 @@ outputClassSize = 10; % using 3-layer architecture, with element size [400, 25, 
 
 %% Debug displayData.m
 displayData(X(1, :));
+
+%% Debug sigmoid.m
+yExpect = 0.67;
+yRun = sigmoid(0.7);
+
+fprintf('y, run %.2f, expect %.2f\n', yRun, yExpect);
+if abs(yRun - yExpect) > 0.01
+    fprintf('ERR\n');
+    return;
+end
+fprintf('sigmoid.m ok\n');
+
+%% Debug sigmoidGradient.m
+gradientExpect = 0.22;
+gradientRun = sigmoidGradient(0.7);
+
+fprintf('gradient, run %.2f, expect %.2f\n', gradientRun, gradientExpect);
+if abs(gradientRun - gradientExpect) > 0.01
+    fprintf('ERR\n');
+    return;
+end
+fprintf('sigmoidGradient.m ok\n');
+
+%% Summary
+fprintf('all ok\n');
