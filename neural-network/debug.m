fa5bcb7028ea7b1ec56e3fe3fadee00da0a02adb
TrentaIcedCoffee <jin@ucdavis.edu>
2018-01-05 19:05:10 -0800

    IMPL forwardPropergate

--- neural-network/debug.m
+++ neural-network/debug.m
@@ -11,12 +11,14 @@ close all;
 clear;
 clc;
 
-load('neural-network.mat');
+load('handwritten-digit.mat');
+load('handwritten-digit-theta.mat');
 m = size(X, 1);
 n = size(X, 2);
 inputElementSize = 400;
 hiddenElementSize = 25;
 outputClassSize = 10; % using 3-layer architecture, with element size [400, 25, 10]
+architecturePara = [inputElementSize, hiddenElementSize, outputClassSize];
 
 %% Debug displayData.m
 displayData(X(1, :));
@@ -42,6 +44,21 @@ if abs(gradientRun - gradientExpect) > 0.01
     return;
 end
 fprintf('sigmoidGradient.m ok\n');
+
+%% Debug forwardPropergate.m
+pointExpect = 1.00;
+hypoMat = forwardPropergate(ThetaCell, X);
+pointRun = hypoMat(1, 10);
+
+fprintf('point, run %.2f, expect %.2f\n', pointRun, pointExpect);
+if abs(pointRun - pointExpect) > 0.01
+    fprintf('ERR\n');
+    return;
+end
+fprintf('forwardPropergate.m ok\n');
+
+%% Debug backwardPropergate.m
+
 
 %% Summary
 fprintf('all ok\n');
