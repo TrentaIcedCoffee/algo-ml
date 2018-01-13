5e669904581c28c330f3e5c3067dec44395c361e
TrentaIcedCoffee <jin@ucdavis.edu>
2018-01-13 15:15:15 -0800

    separate neural_network to an independent repo

--- neural_network/sigmoid.m
+++
@@ -1,9 +0,0 @@
-function y = sigmoid(x)
-%SIGMOID Compute sigmoid on x
-%   sigmoid is continuous and has range(0, 1), easy to analyze
-
-y = zeros(size(x));
-
-y = 1 ./ (1 + exp(-x));
-
-end
--- neural_network/sigmoidGradient.m
+++
@@ -1,8 +0,0 @@
-function gradient = sigmoidGradient(x)
-%SIGMOIDGRADIENT Compute derivative of sigmoid function on x
-
-gradient = zeros(size(x));
-
-gradient = sigmoid(x) .* (1 - sigmoid(x));
-
-end
